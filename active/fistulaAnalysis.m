9390792f7ced6f047806ee92d3a2de8cb5b7f542
Barry Belmont <belmont@umich.edu>
2014-10-02 08:52:14 -0400

    New blank slate for us

---
+++ active/fistulaAnalysis.m
@@ -0,0 +1,23 @@
+%% New Function for Matt Leavitt
+
+
+
+%% Pick two points and track with time
+% Luc
+
+
+
+%% Detect edge
+% Barry
+
+
+%% Track edge
+% Barry
+
+
+%% Block-match/point track entire region of interest for strain
+
+
+
+
+%% Correlation within vessel
