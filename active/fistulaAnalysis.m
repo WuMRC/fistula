4278490b95da9163397f9fc37436756d1c250504
Barry Belmont <belmont@umich.edu>
2014-10-16 10:12:39 -0400

    Small insignificant changes

--- active/fistulaAnalysis.m
+++ active/fistulaAnalysis.m
@@ -90,18 +90,21 @@ imageROI = dicomROI(image_change);
 indFrame = 1;
 while indFrame <= nFrames
     imageROI_adjusted(:,:,indFrame) = imadjust(imageROI(:,:,indFrame));
-    imageROI_level(indFrame) = graythresh(imageROI(:,:,indFrame));
-    imageROI_BW(:,:,indFrame) = im2bw(imageROI(:,:,indFrame),...
+    imageROI_level(indFrame) = graythresh(imageROI_adjusted(:,:,indFrame));
+    imageROI_BW(:,:,indFrame) = im2bw(imageROI_adjusted(:,:,indFrame),...
         imageROI_level(indFrame));
     indFrame = indFrame + 1;
 end
 
-    % An interesting result
-    time = (1:99)./16;
-    plot(time,imageROI_level)
-    xlabel('Time [s]')
-    ylabel('Graythreshold')
-    title('A heartbeat measure by image contrast')
+implay(imageROI_BW)
+
+%     % An interesting result
+%     time = (1:99)./16;
+%     plot(time,imageROI_level)
+%     xlabel('Time [s]')
+%     ylabel('Graythreshold')
+%     title('A heartbeat measure by image contrast')
+
 
 
 
